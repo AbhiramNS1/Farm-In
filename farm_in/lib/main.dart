b017789f301de45357ec28b19884e1f4d9dd1134
Abhiram <abhiramns345@gmail.com>
2023-04-17 22:36:13 -0700

    Project initialized

---
+++ farm_in/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(FarmIn());
+}
+
+class FarmIn extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(body: Center(child: Text("hello world"))),
+    );
+  }
+}
